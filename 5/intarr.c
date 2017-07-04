f7ef5dbca202672ff5854498eb601091fb657bac
gza21 <gza21@sfu.ca>
2017-07-04 03:42:30 +0000

    l15 submit

--- 5/intarr.c
+++ 5/intarr.c
@@ -209,7 +209,7 @@ intarr_result_t intarr_resize( intarr_t* ia, unsigned int newlen ){
     return INTARR_OK;
   }
   
-  newia=realloc(ia->data,newlen*sizeof(int));
+  newia=(int*)realloc(ia->data,newlen*sizeof(int));
   
   if(newia==NULL&&newlen!=0){
     
