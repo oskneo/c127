e2bc8e91e6421fd8f3954a9e90985d114b5ea54c
gza21 <gza21@sfu.ca>
2017-07-28 03:50:29 +0000

    l4

--- 4/t3.c
+++ 4/t3.c
@@ -39,8 +39,9 @@ void life( uint8_t array[],
 {
 	int i,j;
 	
-	uint8_t *ar2=malloc(cols*rows*sizeof(uint8_t));
-	zero(ar2,cols,rows);
+	//uint8_t *ar2=malloc(cols*rows*sizeof(uint8_t));
+	int ar3[10][3],k=0;
+	//zero(ar2,cols,rows);
 	for(i=0;i<cols;i++){
 		for(j=0;j<rows;j++){
 		    int check =deadoralive(array,cols,rows,i,j);
@@ -71,7 +72,11 @@ void life( uint8_t array[],
 		    }
 		    if(check==1){
 		        if(n<2||n>3){
-		            set_pixel(ar2, cols, rows, i,j,0);
+		            //set_pixel(ar2, cols, rows, i,j,1);
+		            ar3[k][0]=i;
+		            ar3[k][1]=j;
+		            ar3[k][2]=1;
+		            k++;
 		            printf("i:%d,j:%d,0.\n",i,j);
 		        }
 		        
@@ -79,7 +84,11 @@ void life( uint8_t array[],
 		    }
 		    else{
 		        if(n==3){
-		            set_pixel(ar2, cols, rows, i,j,255);
+		            //set_pixel(ar2, cols, rows, i,j,2);
+		            ar3[k][0]=i;
+		            ar3[k][1]=j;
+		            ar3[k][2]=2;
+		            k++;
 		            printf("i:%d,j:%d,255.\n",i,j);
 		        }
 		    }
@@ -94,9 +103,21 @@ void life( uint8_t array[],
 	
 	int x=1,y=2;
 	printf("0:%d\n",array[ y*cols + x ]);
-	memcpy(array,ar2,cols*rows*sizeof(uint8_t));
+	//memcpy(array,ar2,cols*rows*sizeof(uint8_t));
+	k--;
+	for(;k>=0;k--){
+		if(ar3[k][2]==1){
+			set_pixel(array, cols, rows, ar3[k][0],ar3[k][1],0);
+		}else{
+			set_pixel(array, cols, rows, ar3[k][0],ar3[k][1],255);
+		}
+	}
+	
+	
+	
+	
 	printf("1:%d\n",array[ y*cols + x ]);
-	printf("2:%d\n",ar2[ y*cols + x ]);
+	//printf("2:%d\n",ar2[ y*cols + x ]);
 	//*array=*ar2;
 		      
 }
